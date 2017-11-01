edbffc01e4b65e72c12c4fca249917dfb5ea50bd
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-11-02 00:40:46 +0200

    Added sync article sync

---
+++ src/main/java/fi/metatavu/soteapi/wordpress/tasks/posts/PostUpdateJob.java
@@ -0,0 +1,123 @@
+package fi.metatavu.soteapi.wordpress.tasks.posts;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.StringUtils;
+
+import fi.metatavu.soteapi.content.ContentController;
+import fi.metatavu.soteapi.persistence.model.Content;
+import fi.metatavu.soteapi.persistence.model.ContentData;
+import fi.metatavu.soteapi.persistence.model.ContentTitle;
+import fi.metatavu.soteapi.persistence.model.ContentType;
+import fi.metatavu.soteapi.tasks.AbstractUpdateJob;
+import fi.metatavu.soteapi.wordpress.WordpressConsts;
+
+public class PostUpdateJob extends AbstractUpdateJob {
+  
+  @Inject
+  private PostUpdateQueue postUpdateQueue;
+  
+  @Inject
+  private ContentController contentController;
+  
+  @Override
+  protected void execute() {
+    PostUpdateTask postUpdateTask = postUpdateQueue.next();
+    if (postUpdateTask != null) {
+      performTask(postUpdateTask);
+    }
+  }
+  
+  private void performTask(PostUpdateTask task) {
+    PostUpdateTaskModel postUpdateModel = task.getPostUpdateModel();
+
+    if (postUpdateModel == null) {
+      return;
+    }
+
+    String originId = postUpdateModel.getOriginId();
+    Content contentEntity = contentController.findContentByOriginId(originId);
+    
+    if (contentEntity != null) {
+      updateExistingPage(contentEntity, postUpdateModel);
+      return;
+    }
+    
+    createNewContent(postUpdateModel);
+  }
+  
+  private void createNewContent(PostUpdateTaskModel postUpdateModel) {
+    if (postUpdateModel == null) {
+      return;
+    } 
+    
+    String originId = postUpdateModel.getOriginId();
+    String slug = postUpdateModel.getSlug();
+    String ContentTitle = postUpdateModel.getTitle();
+    String contentData = postUpdateModel.getContent();
+    String parentOriginId = postUpdateModel.getParentOriginId();
+    Content parent = null;
+    
+    if (StringUtils.isNotEmpty(parentOriginId)) {
+      parent = contentController.findContentByOriginId(parentOriginId);
+      if (parent == null) {
+        return;
+      }
+    }
+    
+    Content contentEntity = contentController.createContent(originId, slug, ContentType.NEWS, parent);
+    
+    if (StringUtils.isNotEmpty(ContentTitle)) {
+      contentController.createContentTitle(WordpressConsts.DEFAULT_LANGUAGE, ContentTitle, contentEntity);
+    }
+
+    if (StringUtils.isNotEmpty(contentData)) {
+      contentController.createContentData(WordpressConsts.DEFAULT_LANGUAGE, contentData, contentEntity);
+    }
+  }
+  
+  private void updateExistingPage(Content contentEntity, PostUpdateTaskModel postUpdateModel) {
+    String parentOriginId = postUpdateModel.getParentOriginId();
+    Content parent = null;
+    
+    if (StringUtils.isNotEmpty(parentOriginId)) {
+      parent = contentController.findContentByOriginId(parentOriginId);
+      if (parent == null) {
+        return;
+      }
+    }
+    
+    contentController.updateContent(contentEntity, postUpdateModel.getOriginId(), postUpdateModel.getSlug(), ContentType.NEWS, parent);
+    String contentTitleContent = postUpdateModel.getTitle();
+    String contentData = postUpdateModel.getContent();
+
+    if (StringUtils.isNotEmpty(contentTitleContent)) {
+      ContentTitle contentTitleEntity = contentController.listContentTitlesByContent(contentEntity)
+        .stream()
+        .filter(title -> title.getLanguage().equals(WordpressConsts.DEFAULT_LANGUAGE))
+        .findAny()
+        .orElse(null);
+      
+      if (contentTitleEntity != null) {
+        contentController.updateContentTitle(contentTitleEntity, WordpressConsts.DEFAULT_LANGUAGE, contentTitleContent, contentEntity);
+      } else {
+        contentController.createContentTitle(WordpressConsts.DEFAULT_LANGUAGE, contentTitleContent, contentEntity);
+      }
+    }
+
+    if (StringUtils.isNotEmpty(contentData)) {
+      ContentData contentDataEntity = contentController.listContentDataByContent(contentEntity)
+        .stream()
+        .filter(content -> content.getLanguage().equals(WordpressConsts.DEFAULT_LANGUAGE))
+        .findAny()
+        .orElse(null);
+      
+      if (contentDataEntity != null) {
+        contentController.updateContentData(contentDataEntity, WordpressConsts.DEFAULT_LANGUAGE, contentData, contentEntity);
+      } else {
+        contentController.createContentData(WordpressConsts.DEFAULT_LANGUAGE, contentData, contentEntity);
+      } 
+    }
+  }
+  
+}
---
+++ src/main/java/fi/metatavu/soteapi/wordpress/tasks/posts/PostUpdater.java
@@ -0,0 +1,33 @@
+package fi.metatavu.soteapi.wordpress.tasks.posts;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.enterprise.concurrent.ManagedScheduledExecutorService;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@Singleton
+@ApplicationScoped
+@Startup
+public class PostUpdater {
+
+  @Inject
+  private PostUpdateJob postUpdateJob;
+  
+  @Resource
+  private ManagedScheduledExecutorService managedScheduledExecutorService;
+  
+  @PostConstruct
+  public void postConstruct() {
+    startTimer(1000, 1000);
+  }
+  
+  private void startTimer(long warmup, long delay) {
+    managedScheduledExecutorService.scheduleWithFixedDelay(postUpdateJob, warmup, delay, TimeUnit.MILLISECONDS);
+  }
+
+}
