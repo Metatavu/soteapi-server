217945e52bbe2a74ef15e45417a27e842d53fcc8
Antti Leppä <antti.leppa@metatavu.fi>
2017-11-02 12:15:14 +0200

    Initial draft of CORS header allow all filter

---
+++ src/main/java/fi/metatavu/soteapi/server/rest/AccessControlAllowOriginFilter.java
@@ -0,0 +1,46 @@
+package fi.metatavu.soteapi.server.rest;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter (urlPatterns = "/v1/*")
+public class AccessControlAllowOriginFilter implements Filter {
+  
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+     // Nothing to init
+  }
+  
+  /**
+   * Adds allows Access-Control-Allow-Origin: * header to all requests
+   * 
+   * @param request request
+   * @param response response
+   * @param chain filter chain
+   * @throws IOException IOException
+   * @throws ServletException  ServletException
+   */
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    if (response instanceof HttpServletResponse) { 
+      HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+      httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+    }
+    
+    chain.doFilter(request, response);
+  }
+
+  @Override
+  public void destroy() {
+    // Nothing to destroy
+  }
+  
+}
