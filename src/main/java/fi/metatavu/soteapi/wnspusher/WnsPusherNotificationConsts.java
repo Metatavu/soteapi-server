1caa798066bea7a45535a72626c18a52319c8ceb
Ilmo Euro <ilmo.euro@gmail.com>
2018-02-13 11:54:25 +0200

    #52 - better key names

--- src/main/java/fi/metatavu/soteapi/wnspusher/WnsPusherNotificationConsts.java
+++ src/main/java/fi/metatavu/soteapi/wnspusher/WnsPusherNotificationConsts.java
@@ -2,8 +2,8 @@ package fi.metatavu.soteapi.wnspusher;
 
 public class WnsPusherNotificationConsts {
 
-  public static final String ENDPOINT_SETTING = "wnspusher.notification.connectionString";
-  public static final String API_KEY_SETTING = "wnspusher.notification.hubName";
+  public static final String ENDPOINT_SETTING = "wnspusher.notification.endpoint";
+  public static final String API_KEY_SETTING = "wnspusher.notification.apiKey";
   public static final String APP_SETTING = "wnspusher.notification.app";
   
   private WnsPusherNotificationConsts() {
