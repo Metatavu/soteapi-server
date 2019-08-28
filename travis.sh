a95ffbb44a9467bfbd05c4cde71e1758897ffa41
Antti Leppä <antti.leppa@metatavu.fi>
2019-08-28 08:12:26 +0300

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn clean verify jacoco:report coveralls:report -Pitests sonar:sonar -DskipCoverage=false -DrepoToken=$COVERALLS_TOKEN
+mvn clean verify jacoco:report coveralls:report -Pitests sonar:sonar -DskipCoverage=false -DrepoToken=$COVERALLS_TOKEN --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
